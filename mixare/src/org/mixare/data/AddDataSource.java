55421c80333e0c8a9f1068af042670fb27038faa
ilCONDOR <ilcondor89@gmail.com>
2013-06-10 19:13:27 +0200

    just fixes: if no DataSources in XML, added Polygon DS does not work!!!

--- mixare/src/org/mixare/data/AddDataSource.java
+++ mixare/src/org/mixare/data/AddDataSource.java
@@ -21,28 +21,28 @@ import com.actionbarsherlock.view.MenuItem;
  * @author KlemensE
  */
 public class AddDataSource extends SherlockActivity {
-	
+
 	private static final int MENU_SAVE_ID = Menu.FIRST;
-	
+
 	EditText nameField;
 	EditText urlField;
 	Spinner typeSpinner;
 	Spinner displaySpinner;
 	Spinner blurSpinner;
-	
+
 	Bundle extras;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.datasourcedetails);
-		
+
 		nameField = (EditText) findViewById(R.id.name);
 		urlField = (EditText) findViewById(R.id.url);
 		typeSpinner = (Spinner) findViewById(R.id.type);
 		displaySpinner = (Spinner) findViewById(R.id.displaytype);
 		blurSpinner = (Spinner) findViewById(R.id.blurtype);
-		
+
 		extras = getIntent().getExtras();
 		if (extras != null) {
 			// Get DataSource
@@ -55,7 +55,7 @@ public class AddDataSource extends SherlockActivity {
 				displaySpinner.setSelection(ds.getDisplayId());
 				blurSpinner.setSelection(ds.getBlurId());
 			}
-			
+
 			// Check whether DataSource can be edited or not
 			if (extras.containsKey("isEditable")) {
 				boolean activated = extras.getBoolean("isEditable");
@@ -69,10 +69,10 @@ public class AddDataSource extends SherlockActivity {
 				displaySpinner.setClickable(activated);
 			}
 		}
-		
+
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
-	
+
 	/**
 	 * Creates a new DataSource and Saves it to the SharedPreferences
 	 */
@@ -85,7 +85,7 @@ public class AddDataSource extends SherlockActivity {
 				.getSelectedItemPosition());
 		int blurId = (int) blurSpinner.getItemIdAtPosition(blurSpinner
 				.getSelectedItemPosition());
-		
+
 		if (!name.isEmpty() && !url.isEmpty()) {
 			if (extras != null) {
 				if (extras.containsKey("DataSourceId")) {
@@ -97,7 +97,7 @@ public class AddDataSource extends SherlockActivity {
 					ds.setType(typeId);
 					ds.setDisplay(displayId);
 					ds.setBlur(blurId);
-					
+
 					DataSourceStorage.getInstance(getApplicationContext()).save();
 					return true;
 				}
@@ -113,7 +113,7 @@ public class AddDataSource extends SherlockActivity {
 			return false;
 		}
 	}
-	
+
 	@Override
 	public boolean onMenuItemSelected(int featureId, com.actionbarsherlock.view.MenuItem item) {
 		switch (item.getItemId()) {
@@ -130,7 +130,7 @@ public class AddDataSource extends SherlockActivity {
 		}
 		return super.onMenuItemSelected(featureId, item);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu) {	
 		menu.add(0, MENU_SAVE_ID, Menu.NONE, "Save").setShowAsAction(
